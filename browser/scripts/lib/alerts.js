4cdede5751dd11ffdedf504733219dd924c7149d
lski <lee.cooper@lski.uk>
2016-04-03 15:02:38 +0100

    Updated

--- browser/scripts/lib/alerts.js
+++
@@ -1,2 +0,0 @@
-!function(n){"use strict";"function"==typeof define&&define.amd?define([],n):(window.lski=window.lski||{},window.lski.alerts=n())}(function(){var n={alert:function(n,i){setTimeout(function(){window.alert(n),"function"==typeof i&&i()},1)},confirm:function(n,i){setTimeout(function(){i(window.confirm(n))},1)}};return n});
-//# sourceMappingURL=../../../maps/browser/scripts/lib/alerts.js.map
